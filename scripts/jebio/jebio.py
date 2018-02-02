1233a404d7a6ef795cf99d4b6d2d88b62031b7a1
Nicolas Falliere <nico@pnfsoftware.com>
2018-02-01 16:31:05 -0800

    JEB.IO Python script: added batch processing (hash list in file) and other options

--- scripts/jebio/jebio.py
+++ scripts/jebio/jebio.py
@@ -4,12 +4,13 @@ What is it? Go read https://www.pnfsoftware.com/blog/introducing-the-jeb-malware
 
 Operations supported: check a file hash, upload a file, download a file.
 This file can be used as a library or as a stand-alone script. In the latter case,
-make sure to update the APIKEY global variable or set up a JEBIO_AIKEY environment variable.
+make sure to update the APIKEY global variable or set up a JEBIO_APIKEY environment variable.
 
 Dependency: requests ('pip install requests' if you don't have it)
 
 Version 1 - Oct 12 2017 (Nicolas Falliere)
 '''
+import getopt
 import json
 import os
 import re
@@ -40,7 +41,7 @@ def check(h, apikey=''):
   r = requests.get(url)  
   return r.json()
 
-def download(h, outpath=None, apikey=''):
+def download(h, outpath=None, apikey='', extract=False):
   if not h:
     raise 'A hash must be provided'
   url = '%s/file/download?apikey=%s&h=%s' % (BASE, getApikey(apikey), h)
@@ -56,10 +57,13 @@ def download(h, outpath=None, apikey=''):
     outpath = h + '.zip'
   with open(outpath, 'wb') as f:
     f.write(r.content)
-  #if extract:  
-  #  from zipfile import ZipFile
-  #  zipfile = ZipFile(outputname)
-  #  zipfile.extractall(pwd='infected')
+  if extract:
+    from zipfile import ZipFile
+    with ZipFile(outpath) as zipfile:
+      zipfile.extractall(pwd='infected')
+      outpath2 = zipfile.namelist()[0]
+    os.unlink(outpath)
+    outpath = outpath2
   return outpath
 
 def upload(filepath, apikey=''):  
@@ -69,12 +73,20 @@ def upload(filepath, apikey=''):
   return r.json()
 
 def usage():
-  print('JEB.IO/"Malware Sharing Network" back-end API wrapper (c) PNF Software, 2017.')
+  p = os.path.split(sys.argv[0])[-1]
+  print('JEB.IO/"Malware Sharing Network" back-end API wrapper (c) PNF Software, 2017-2018.')
   print('This file can be used as a library or as a stand-alone script to check file hashes as well as download and upload files.')
   print('Usage:')
-  print('  %s [check|download|upload] <filehash|filepath>' % os.path.split(sys.argv[0])[-1])
+  print('  %s mode options' % p)
+  print('  where mode is one of \'check\', \'download\', or \'upload\'')
+  print('Details:')
+  print('  %s (check|download|upload) [-d] <filehash|filepath>' % p)
+  print('  %s (check|download) [-d] -f <file.txt>' % p)
+  print('Options:')
+  print('  -v  : extra verbose')
+  print('  -x  : extract downloaded files (only in \'download\' mode')
   print('Example:')
-  print('  %s check 42aaa93a894a69bfcbc21823b09e4ea9f723c428' % os.path.split(sys.argv[0])[-1])
+  print('  %s check 42aaa93a894a69bfcbc21823b09e4ea9f723c428' % p)
   sys.exit(-1)
 
 if __name__ == '__main__':
@@ -82,25 +94,60 @@ if __name__ == '__main__':
     usage()
     sys.exit()
 
+  # retrieve the mode
   action = sys.argv[1].lower()
+
+  try:
+    opts, args = getopt.getopt(sys.argv[2:], 'vxf:')
+  except getopt.GetoptError as err:
+    usage()
+
+  verbose = False
+  extract = False
+  hlist = []
+  for o, a in opts:
+    if o == '-v':
+      verbose = True
+    elif o == '-x':
+      extract = True
+    elif o == '-f':
+      with open(a) as f:
+        for line in f.readlines():
+          line = line.strip()
+          if line and not line.startswith('#'):
+            hlist.append(line)
+    else:
+      usage()
+
+  hlist.extend(args)
+  hlist = list(set(hlist))
+
+  if verbose:
+    print('Mode: %s' % action)
+    print('  (extract=%s)' % extract)
+    print('Processing %d entries' % len(hlist))
+
   if action == 'check':
-    for h in sys.argv[2:]:
+    for h in hlist:
       try:
         print('%s: %s' % (h, json.dumps(check(h), indent=4, sort_keys=True)))
       except Exception as e:
         traceback.print_exc()
   elif action == 'download':
-    for h in sys.argv[2:]:
+    for h in hlist:
       try:
-        outpath = download(h)
+        outpath = download(h, extract=extract)
         if outpath:
-          print('%s: downloaded to %s (password: "infected")' % (h, outpath))
+          if extract:
+            print('%s: downloaded to %s' % (h, outpath))
+          else:
+            print('%s: downloaded to %s (password: "infected")' % (h, outpath))
         else:
           print('%s: NOT found' % h)
       except Exception as e:
         traceback.print_exc()
   elif action == 'upload':
-    for filepath in sys.argv[2:]:
+    for filepath in hlist:
       try:
         print('%s: %s' % (filepath, json.dumps(upload(filepath), indent=4, sort_keys=True)))
       except Exception as e:
