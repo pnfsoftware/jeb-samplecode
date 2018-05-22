dc6c0b1adff25ffac05d060b89b15f8b819094c1
Nicolas Falliere <nico@pnfsoftware.com>
2018-05-22 10:22:52 -0700

    JEB scripts to save and reapply basic ICodeUnit refactoring data (eg, comments, renamed items)

---
+++ scripts/JEB2CodeLoad.py
@@ -0,0 +1,72 @@
+"""
+JEB script to reload and apply basic refactoring data onto loaded code units.
+- Data file: [JEB]/bin/codedata.txt
+- See converse script to persist such data on disk: JEB2CodeSave.py
+
+Requires: JEB 2.3.14.20180522+
+
+Refer to SCRIPTS.TXT for more information.
+"""
+
+import json
+import os
+import time
+
+from com.pnfsoftware.jeb.client.api import IScript
+from com.pnfsoftware.jeb.core import RuntimeProjectUtil
+from com.pnfsoftware.jeb.core.units.code import ICodeUnit
+
+
+class JEB2CodeLoad(IScript):
+  def run(self, ctx):
+    engctx = ctx.getEnginesContext()
+    if not engctx:
+      print('Back-end engines not initialized')
+      return
+
+    projects = engctx.getProjects()
+    if not projects:
+      print('There is no opened project')
+      return
+
+    prj = projects[0]
+    prjname = prj.getName()
+
+    prgdir = ctx.getProgramDirectory()
+    datafile = os.path.join(prgdir, 'codedata.txt')
+    data = {}
+    if os.path.exists(datafile):
+      with open(datafile, 'r') as f:
+        try:
+          data = json.load(f)
+        except:
+          pass
+    print('Current data:', data)
+
+    d = data.get(prjname, None)
+    if not d:
+      print('Nothing to reload')
+      return
+
+    units = RuntimeProjectUtil.findUnitsByType(prj, ICodeUnit, False)
+    if not units:
+      print('No code unit available')
+      return
+
+    for unit in units:
+      a = d.get(unit.getName(), None)
+      if a:
+        comments = a['comments']
+        renamed_classes = a['renamed_classes']
+        renamed_fields = a['renamed_fields']
+        renamed_methods = a['renamed_methods']
+        for addr, comment in comments.items():
+          unit.setComment(addr, comment)
+        for sig, name in renamed_classes.items():
+          unit.getClass(sig).setName(name)
+        for sig, name in renamed_fields.items():
+          unit.getField(sig).setName(name)
+        for sig, name in renamed_methods.items():
+          unit.getMethod(sig).setName(name)
+
+    print('Basic refactoring data was applied')
---
+++ scripts/JEB2CodeSave.py
@@ -0,0 +1,91 @@
+"""
+JEB script to save (persist) basic refactoring data of currently loaded code units.
+- Data file: [JEB]/bin/codedata.txt
+- See converse script to load and apply that data onto a fresh project: JEB2CodeLoad.py
+
+Requires: JEB 2.3.14.20180522+
+
+Refer to SCRIPTS.TXT for more information.
+"""
+
+import json
+import os
+import time
+
+from com.pnfsoftware.jeb.client.api import IScript
+from com.pnfsoftware.jeb.core import RuntimeProjectUtil
+from com.pnfsoftware.jeb.core.units.code import ICodeUnit
+
+
+class JEB2CodeSave(IScript):
+  def run(self, ctx):
+    engctx = ctx.getEnginesContext()
+    if not engctx:
+      print('Back-end engines not initialized')
+      return
+
+    projects = engctx.getProjects()
+    if not projects:
+      print('There is no opened project')
+      return
+
+    prj = projects[0]
+    prjname = prj.getName()
+
+    prgdir = ctx.getProgramDirectory()
+    datafile = os.path.join(prgdir, 'codedata.txt')
+    data = {}
+    if os.path.exists(datafile):
+      with open(datafile, 'r') as f:
+        try:
+          data = json.load(f)
+        except:
+          pass
+    #print('Current data:', data)
+
+    units = RuntimeProjectUtil.findUnitsByType(prj, ICodeUnit, False)
+    if not units:
+      print('No code unit available')
+      return
+
+    d = {}
+    for unit in units:
+      a = {}
+      d[unit.getName()] = a
+
+      commentsmap = unit.getComments()
+      a['comments'] = {}
+      a['renamed_classes'] = {}
+      a['renamed_fields'] = {}
+      a['renamed_methods'] = {}
+
+      for addr, comment in commentsmap.items():
+        a['comments'][addr] = comment
+
+      for c in unit.getClasses():
+        name0 = c.getName(False)
+        name1 = c.getName(True)
+        if name0 != name1:
+          a['renamed_classes'][c.getSignature(False)] = name1
+
+      for f in unit.getFields():
+        name0 = f.getName(False)
+        name1 = f.getName(True)
+        if name0 != name1:
+          a['renamed_fields'][f.getSignature(False)] = name1
+
+      for m in unit.getMethods():
+        name0 = m.getName(False)
+        name1 = m.getName(True)
+        if name0 != name1:
+          a['renamed_methods'][m.getSignature(False)] = name1
+
+    data[prjname] = d
+
+    with open(datafile, 'w') as f:
+      try:
+        json.dump(data, f, indent=True)
+      except Exception as e:
+        print('ERROR: Cannot save to file: %s' % e)
+
+    print('Basic refactoring data recorded to file: %s' % datafile)
