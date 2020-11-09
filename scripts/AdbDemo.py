1aa9761e5214ca53bc08a99ab71531aa800da25b
Nicolas Falliere <nico@pnfsoftware.com>
2020-11-09 09:41:33 -0800

    adb utils and android debug attach sample scripts for jeb 3.26

---
+++ scripts/AdbDemo.py
@@ -0,0 +1,28 @@
+from com.pnfsoftware.jeb.client.api import IScript
+from com.pnfsoftware.jeb.core.units.code.android.adb import AdbWrapperFactory, AdbWrapperFactory, AndroidDeviceUtil
+"""
+Script for JEB Decompiler.
+This script shows how to create and use Android Debug Bridge (adb) wrappers and adb utility objects.
+Reference: AdbWrapperFactory, AdbWrapper, AndroidDeviceUtil
+"""
+class AdbDemo(IScript):
+  def run(self, ctx):
+    # factory object wraps adb, not tied to a specific device
+    adbf = AdbWrapperFactory()  # see javadoc for a list of methods
+    adbf.initialize()
+    print('adb version: %s' %adbf.getVersion())
+
+    for dev in adbf.listDevices():
+      # an AdbWrapper object is tied to a debuggable device
+      adb = adbf.createWrapper(dev.getSerial())
+      break
+    else:
+      print 'no android device found'
+      return
+    print(adb) # see javadoc for a list of methods
+    #adb.listPackages()
+    #adb.listProcesses()
+    #adb.readProperty("ro.build.version.release")
+    #adb....    
+
+    # higher-level wrappers: see AndroidDeviceUtil
---
+++ scripts/AndroidDbgCreate.py
@@ -0,0 +1,57 @@
+from com.pnfsoftware.jeb.client.api import IScript
+from com.pnfsoftware.jeb.core.units.code.android import IApkUnit, IDexUnit
+from com.pnfsoftware.jeb.core.units.code.debug.impl import DebuggerSetupInformation
+from com.pnfsoftware.jeb.core.util import DebuggerHelper
+"""
+Script for JEB Decompiler.
+This script demonstrates how to create a debugger unit for an APK object.
+"""
+class AndroidDbgCreate(IScript):
+  def run(self, ctx):
+    prj = ctx.getMainProject()
+    if not prj:
+      print 'No project'
+      return
+
+    # let's retrieve the primary apk unit loaded in the project, if there is one
+    apk = prj.findUnit(IApkUnit)
+    if not prj:
+      print 'No APK opened'
+      return
+
+    # a debugger unit is the child of another unit
+    dbg = DebuggerHelper.getDebugger(apk, True)
+    if not dbg:
+      print 'Cannot create or retrieve debugger'
+      return
+
+    # after creation, the debugger unit is not attached to any target
+    print(dbg)
+
+    if not dbg.isAttached():
+      # we will find a matching compatible target on the devices that can be enumerated by the debugger unit
+      setup = findTarget(dbg, apk)
+      if not setup:
+        print 'Target not found'
+        return
+      # UNCOMMENT below to request all threads to be stopped before attaching
+      #setup.setSuspendThreads(True)
+      # UNCOMMENT below to allow the creation of a child debugger for APK's native code
+      #setup.setUseChildrenDebuggers(True)
+      dbg.attach(setup)
+
+    print 'Debugger found and attached: %s' % dbg
+
+
+def findTarget(dbg, apk):
+  # here, we use the un-attached debugger unit to enumerate compatible targets,
+  # enumerate processes on those targets, and find one that matches our apk
+  # this process may seem convoluted, but keep in mind that IDebuggerUnit are
+  # not specific to android, they are a generic interface for any type of debugger
+  te = dbg.getTargetEnumerator()
+  for m in te.listMachines():
+    for p in m.getProcesses():
+      # match by exact package name
+      if p.getName() == apk.getPackageName():
+        return DebuggerSetupInformation.create(m, p)
+  return None
