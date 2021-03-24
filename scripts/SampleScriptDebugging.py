02e9046cab27769f95ef4bda181cdaf9273ceb21
Nicolas Falliere <nico@pnfsoftware.com>
2021-03-24 16:28:47 -0700

    Update

--- scripts/SampleScriptDebugging.py
+++ scripts/SampleScriptDebugging.py
@@ -1,4 +1,4 @@
-#?description=How to use PyDev and pydevd to debug JEB Python scripts
+#?description=How to use pydevd to debug JEB Python scripts inside Eclipse IDE or PyCharm
 #?shortcut=
 from com.pnfsoftware.jeb.client.api import IScript
 """
@@ -7,26 +7,38 @@ Sample script for JEB Decompiler..
 class SampleScriptDebugging(IScript):
   def run(self, ctx):
 
-    # To be done once:
-    # 1) Install Eclipse IDE and the PyDev plugin (PyCharm users: look into https://pypi.org/project/pydevd-pycharm/)
-    # 2) Git-clone https://github.com/fabioz/PyDev.Debugger into your JEB folder
+    # To debug inside Eclipse IDE:
+    #   1) Install Eclipse IDE and the PyDev plugin to make it a full-fledged Python IDE
+    #   2) Git-clone https://github.com/fabioz/PyDev.Debugger into your JEB folder
+    # To debug inside PyCharm:
+    #   1) Install JetBrains PyCharm
+    #   2) Download pydevd-pycharm from https://pypi.org/project/pydevd-pycharm/#files and
+    #      unpack the pydevd-pycharm-xxx directory buried in the tarball to a newly
+    #      created `pydevd-pycharm` folder into your JEB folder
 
-    # To be done to get ready to debug a script:
-    # 3) Fire up the IDE, switch to the Debug perspective, then select Pydev menu, Start Debug Server...
-    # 4) In the script to be debugged, import pydevd:
+    # Now get ready to debug a script:
+    # 1) Fire up the IDE, switch to the Debug perspective/layout and start the PyDev Debug Server.
+    # 2) In the script to be debugged, import the appropriate pydevd module:
+
+    # ... with Eclipse:
     import sys
     sys.path.append(ctx.getBaseDirectory() + '/PyDev.Debugger')
     import pydevd
+    # ... or with PyCharm: (uncomment below and comment above)
+    #sys.path.append(ctx.getBaseDirectory() + '/pydevd-pycharm')
+    #import pydevd_pycharm
 
-    # 5) Wherever the breakpoint is to be placed, call settrace()
-    pydevd.settrace()
+    # 3) Wherever the breakpoint is to be placed, call settrace()
+    # note: additional parameters may be specified, such as the debugger server hostname/port, etc.
+    # refer to pydevd documentation for details
+    pydevd.settrace(stdoutToServer=True, stderrToServer=True)
 
-    # 6) Now, run your script (e.g. within JEB GUI, press F2, then select or browse for your script, and execute it)
+    # 4) Now, run your script (e.g. within JEB GUI, press F2, then select or browse for your script, and execute it)
     # With settrace invoked just above, when the script is run, the debugger will be brought up when execution reaches
     # the statement that follows it, in the case of this example, that one:
     a = 1
 
-    # 7) Switch to your IDE; your script should have been brought up in the editor, with the current on-breakpoint line
+    # 5) Switch to your IDE; your script should have been brought up in the editor, with the current on-breakpoint line
     # highlighted in green. Debug as expected (tracing, variable views/writes, etc.). Note that rendering of print() statements
     # will be delayed in JEB's logger; you can see them if JEB was started from a shell rendering stdout
 
