6c1fc2ecb8eafe2d9a4cc7b226a56a271f1defb4
cw2k <3834079+cw2k@users.noreply.github.com>
2024-10-21 01:33:45 +0200

    Update CodeLoad.py
    Better error handling
    Load now also works, when the option 
    'parsers/apk/merge muli Dex' was chance.

--- scripts/CodeLoad.py
+++ scripts/CodeLoad.py
@@ -9,57 +9,173 @@ from com.pnfsoftware.jeb.core.units.code import ICodeUnit
 """
 Sample script for JEB Decompiler.
 
-JEB script to reload and apply basic refactoring data onto loaded code units.
-- Data file: [JEB]/bin/codedata.txt
-- See converse script to persist such data on disk: CodeSave.py
+    JEB script to load/deserialize and apply basic refactoring data onto loaded code units.
+    - On each unit of the main project it'll
+      rename classes, fields and methods and add comments by it's address
+    - Input data file: [JEB]/bin/codedata.txt
+    - It's complementary script is: CodeSave.py . It'll save/serialize such data onto disk.
+
+
 """
 class CodeLoad(IScript):
-
   def run(self, ctx):
     prj = ctx.getMainProject()
-    assert prj, 'Need a project'
+    assert prj, 'getMainProject() failed:' 'Need a project'
+
+    dataFileName = os.path.join( ctx.getProgramDirectory(), 'codedata.txt' )
+
+    def OpenDatFile (fileName, prjname ):
+        """
+        Opens jsonFile and returns JSON-node with prjname
+        """
+        data = {}
+        if os.path.exists(fileName):
+          with open(fileName, 'r') as f:
+            try:
+              data = json.load(f)
+            except:
+              pass
+        print '\nCurrent data:', json.dumps( data, indent=2  )
+
+        print 'MainProjectName:', prjname
+
+        return data.get(prjname, None)
 
     prjname = prj.getName()
+    d = OpenDatFile( dataFileName,
+                     prjname )
+    assert d, 'There is nothing to load since the file is empty.'
 
-    prgdir = ctx.getProgramDirectory()
-    datafile = os.path.join(prgdir, 'codedata.txt')
-    data = {}
-    if os.path.exists(datafile):
-      with open(datafile, 'r') as f:
-        try:
-          data = json.load(f)
-        except:
-          pass
-    print('Current data:', data)
+    # Get units from project
+    units = RuntimeProjectUtil.findUnitsByType( prj, ICodeUnit, False )
+    assert units, 'In the project there are no code unit available.'
 
-    d = data.get(prjname, None)
-    if not d:
-      print('Nothing to reload')
-      return
+    unit_name_last=""
 
-    units = RuntimeProjectUtil.findUnitsByType(prj, ICodeUnit, False)
-    if not units:
-      print('No code unit available')
-      return
-
+    # For all units in the project...
     for unit in units:
-      if not unit.isProcessed():
-        continue
 
-      a = d.get(unit.getName(), None)
-      if a:
-        renamed_classes = a['renamed_classes']
-        renamed_fields = a['renamed_fields']
-        renamed_methods = a['renamed_methods']
-        comments = a['comments']
-        for sig, name in renamed_classes.items():
-          unit.getClass(sig).setName(name)
-        for sig, name in renamed_fields.items():
-          unit.getField(sig).setName(name)
-        for sig, name in renamed_methods.items():
-          unit.getMethod(sig).setName(name)
+        # skip unprocessed units
+        if not unit.isProcessed():
+            continue
+
+        unit_Name = unit.getName()
+        print "unit_Name: ", unit_Name
+
+        # open unit in data file
+        unit_data = d.get(unit_Name)
+        if not unit_data:
+            if unit_name_last:
+                print (
+                    "For the unit '" + unit_Name + "' there are no entries in the data file. \n"
+                    "    Maybe that is because the option 'parsers/apk/merge muli Dex' was changed \n"
+                    "Trying unit_Name: '" + unit_name_last + "' instead."
+                )
+                unit_data = d.get(unit_name_last)
+            assert unit_data, "For the unit '" + unit_Name + "' there are no entries in the data file."
+        else:
+            unit_name_last=unit_Name
+
+        def ProcessItems( items, item_func ):
+            for item, name in IForEachWithStats(
+                                                    unit_data[items],
+                                                    item_func,
+                                                    items.split("_")[1] ):
+                item.setName(name)      if item else None
+
+
+        # #Version for people who don't like iterators:
+        # # Process classes
+        #for item, name in unit_data['renamed_classes'].items():
+        #   item = unit.getClass(item)
+        #   if item:
+        #       item.setName(name)
+
+        # Process classes
+        ProcessItems( 'renamed_classes', unit.getClass )
+
+        # Process fields
+        ProcessItems( 'renamed_fields', unit.getField )
+
+        # Process methods
+        ProcessItems( 'renamed_methods', unit.getMethod )
+
         # note: comments are applied last since `addr` can be a refactored one here
-        for addr, comment in comments.items():
+        for addr, comment in unit_data['comments'].items():
           unit.setPrimaryComment(addr, comment)
 
-    print('Basic refactoring data was applied')
+        print(
+                '\n'   +
+                '='*80 +
+                '\n'
+
+        )
+    print 'Basic refactoring data was applied'
+
+
+
+
+
+
+
+
+
+
+class IForEachWithStats:
+    """
+    An iterator class to process items and keep track of statistics.
+
+    Attributes:
+        items      (dict)   : A dictionary of items to process.
+        item_func (function): A function to process each item.
+        item_type (str)     : A string representing the type of items being processed.
+
+    Methods:
+        __iter__(): Initializes the iterator and resets statistics.
+        __next__(): Processes the next item, updates statistics, and prints the status.
+    """
+
+    printIndent = '   '
+
+    def __init__(self, items, item_func, item_type):
+        self.items = items
+        self.item_func = item_func
+        self.item_type = item_type
+
+        # Initialize statistics
+        self.stat_total = len(self.items)   # The total number of items
+        self.stat_done = 0                  # The number of successfully processed items
+
+
+        self.next = self.__next__ # Quirk: to make Python2 happy
+
+    def __iter__(self):
+        self._iterator = iter(self.items.items())
+
+
+        print  '_'*80
+        print  self.item_type
+
+        return self
+
+    def __next__(self):
+        try:
+            sig, name = next(self._iterator)
+        except StopIteration:
+
+            print(  IForEachWithStats.printIndent + '^- missing ' + self.item_type + ' are above' ) if self.stat_total<> self.stat_done else None
+            print(  'Stats restored ' + self.item_type + ' '
+                    'done/total: '    + str(self.stat_done) + '/' + str(self.stat_total))
+            raise StopIteration
+
+        item = self.item_func(sig)
+
+        if item:
+            self.stat_done += 1
+        else:
+            print( IForEachWithStats.printIndent +
+                        sig.replace( '->', '\t->\t')
+                           .replace( ')',  ')\t'   )
+                  )
+
+        return item, name
